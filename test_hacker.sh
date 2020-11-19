0e7317791746570aed44fc5cba3ff3e4a3e396df
Borja D <borjanakamura@yahoo.es>
2020-11-19 20:29:12 +0100

    ahora si que funciona el diccionario como deberia

--- test_hacker.sh
+++ test_hacker.sh
@@ -8,11 +8,11 @@ declare -A repos
 repos[campechanos]='https://github.com/rgomezh/campechano.git'
 repos[goodshellers]='https://github.com/ruizber23/goodshellers.git'
 repos[fantasticos]='https://github.com/hxn241/Fantasticos.git'
-repos[equipo_confinados] = 'https://github.com/danielgarciasanchez/Equipo_confinados'
+repos[equipo_confinados]='https://github.com/danielgarciasanchez/Equipo_confinados'
 repos[team5]='https://github.com/rbarlaam/Team5'
 repos[los bashers]='https://github.com/JoaquinLou/Los-Bashers.git'
 
 # Prompt preguntando por el equipo
 read -p "Nombre del equipo a comparar: " team
-echo repos[$team]
+echo ${repos[$team]}
 #git clone repos[$team] ref_repo
