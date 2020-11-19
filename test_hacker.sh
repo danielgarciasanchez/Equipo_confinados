bd88fb2810d2859692b71ee81523ad344745a64d
Borja D <borjanakamura@yahoo.es>
2020-11-19 20:20:29 +0100

    listo para probar si funciona el diccionario correctamente

--- test_hacker.sh
+++ test_hacker.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Crear nueva carpeta ref_repo
-echo mkdir ./ref_repo
-cd ref_repo
+mkdir ./ref_repo
 
 # Dictcionarios con direcciones de los equipos
-echo declare -A repos
-echo repos[campechanos]='github.com/rgomezh/campechano.git'
-echo repos[goodshellers]='github.com/ruizber23/goodshellers.git'
-echo repos[fantasticos]='github.com/hxn241/Fantasticos.git'
-echo repos[equipo_confinados] = 'github.com/danielgarciasanchez/Equipo_confinados'
-echo repos[team5]='github.com/rbarlaam/Team5'
-echo repos[los bashers]='github.com/JoaquinLou/Los-Bashers.git'
+declare -A repos
+repos[campechanos]='https://github.com/rgomezh/campechano.git'
+repos[goodshellers]='https://github.com/ruizber23/goodshellers.git'
+repos[fantasticos]='https://github.com/hxn241/Fantasticos.git'
+repos[equipo_confinados] = 'https://github.com/danielgarciasanchez/Equipo_confinados'
+repos[team5]='https://github.com/rbarlaam/Team5'
+repos[los bashers]='https://github.com/JoaquinLou/Los-Bashers.git'
 
 # Prompt preguntando por el equipo
-echo read -p "Nombre del equipo a comparar: " team
-echo git clone repos[$team]
+read -p "Nombre del equipo a comparar: " team
+echo repos[$team]
+#git clone repos[$team] ref_repo
